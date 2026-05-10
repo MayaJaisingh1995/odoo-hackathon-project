8cfbdf52849f38160f877a49fa8f3534c3688f8a
Durgadevi.M <lantanaphoenix@gmail.com>
2026-05-10 15:52:17 +0530

    Add login functionality to Traveloop app

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-console.log("Traveloop App Started");
-alert("Welcome To Traveloop"); 
+function loginUser(){
+
+    let email =
+    document.getElementById("email").value;
+
+    let password =
+    document.getElementById("password").value;
+
+    if(email === "maya@gmail.com"
+    && password === "1234"){
+
+        localStorage.setItem("user", email);
+
+        alert("Login Successful");
+
+        window.location.href = "index.html";
+
+    }
+
+    else{
+
+        alert("Invalid Email or Password");
+
+    }
+
+}
