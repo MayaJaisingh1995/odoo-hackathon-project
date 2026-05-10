eb871cff34847544901ca06d698eaac0c6993e87
Durgadevi.M <lantanaphoenix@gmail.com>
2026-05-10 13:27:53 +0530

    Add Travel Trip model with fields

--- *init*.py
+++ *init*.py
@@ -1,1 +1,12 @@
+from import models, fields 
+class Travel Trip (models. Model):
+    _name = 'traveloop.trip'
+    _description = 'Travel Trip'
 
+    name = fields .Char (string='Trip Name',
+required=True)
+    start_date = fields .Date(string='start
+Date')    
+    end_date = fields. Date(string='End
+Date')
+    budget = fields.Float(string='Budget')
